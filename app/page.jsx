297fe27e72f669fedfca2b6d0b969146616debc2
lukegronert@gmail.com <lukegronert@gmail.com>
2024-04-01 06:14:35 -0700

    feat: added text to the home page

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container m-auto border-2">
-      <h1>Home</h1>
-      <Link href="/addFriend">ADD FRIEND</Link>
+    <div className="container m-auto">
+      <h2 className="text-2xl text-center">Less stress when sharing expenses <span className="text-teal-400">with housemates.</span></h2>
     </div>
   );
 }
