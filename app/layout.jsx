09c8c4e4606f144fad7591a43cb15fd73c7d64aa
lukegronert@gmail.com <lukegronert@gmail.com>
2024-04-04 02:36:24 -0700

    feat: created login page layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import "./globals.css";
 
 const poppins = Poppins({
-  weight: ['400', '700'],
+  weight: ['400', '500', '600', '700'],
   subsets: ['latin']
 })
 
